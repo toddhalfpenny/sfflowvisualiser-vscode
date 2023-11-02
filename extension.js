995b03795f2ed439a870af125f3da5ebbbe8dcf2
Todd Halfpenny <todd@toddhalfpenny.com>
2023-11-02 11:50:31 +0000

    Initial hacking to get a flow processed to plantuml and then call jebbs preview extension

--- extension.js
+++ extension.js
@@ -1,7 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
-const { XMLParser, XMLBuilder, XMLValidator} = require("fast-xml-parser");
+const { XMLParser } = require("fast-xml-parser");
 
 
 // This method is called when your extension is activated
@@ -12,22 +12,8 @@ const { XMLParser, XMLBuilder, XMLValidator} = require("fast-xml-parser");
  */
 function activate(context) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "flow2plantuml" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('flow2plantuml.generatePlantUML', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Flow2PlantUML!');
-		let xmlData = vscode.window.activeTextEditor.document.getText();
-		const parser = new XMLParser();
-		let jsonObj = parser.parse(xmlData);
-		console.log(jsonObj);
+		generatePlantUMLPreview();
 	});
 
 	context.subscriptions.push(disposable);
@@ -35,6 +21,140 @@ function activate(context) {
 
 // This method is called when your extension is deactivated
 function deactivate() {}
+
+async function generatePlantUMLPreview() {
+	const uml = await generatePlantUML();
+
+	// Create tempfile with plant UML contents
+	let setting = vscode.Uri.parse("untitled:" + "flow2plantuml.tmp");
+	try {
+		const doc = await vscode.workspace.openTextDocument(setting);
+		const e = await vscode.window.showTextDocument(doc, 1, false);
+		e.edit(edit => {
+			edit.insert(new vscode.Position(0, 0), uml);
+
+			// Run plantuml.preview (this calls Jebbs' extension)
+			vscode.commands.executeCommand("plantuml.preview");
+		});
+	} catch (error) {
+		console.error(error);
+		debugger;
+	};
+
+}
+
+async function generatePlantUML() {
+	// Get Flow XML and parse to JSON
+	const xmlData = vscode.window.activeTextEditor.document.getText();
+	const parser = new XMLParser();
+	const flowObj = parser.parse(xmlData).Flow;
+	console.log(JSON.stringify(flowObj));
+	const flowMap = await createFlowMap(flowObj);
+	console.log(JSON.stringify(flowMap));
+
+	const START_STR = "' THIS IS A TEMPORARY FILE\n@startuml\nstart\n";
+	const TITLE_STR = "title " + flowMap['label'] + "\n";
+	let nextNode = flowMap[flowMap['start'].connector.targetReference];
+	let end = false;
+	let bodyStr = '';
+	let i = 0;
+	while (!end && i < 10) {
+		console.log("nextNode", nextNode);
+		bodyStr += getNodeStr(nextNode, flowMap);
+		if (!nextNode.nextNode) {
+			end = true;
+		} else {
+			nextNode = flowMap[nextNode.nextNode]
+		}
+		i++;
+	}
+	const END_STR = "stop\n@enduml";
+	return START_STR + TITLE_STR + bodyStr + END_STR;
+}
+
+async function createFlowMap(flowObj) {
+	let flowMap = {};
+	for (const property in flowObj) {
+		switch (property) {
+			case 'description' :
+			case 'label' :
+			case 'start' :
+				flowMap[property] = flowObj[property];
+				break;
+			default :
+				// If only one entry (e.g one loop) then it will be an object, not an Array, so make it an Array of one
+				if (!flowObj[property].length) { 
+					flowObj[property] = [flowObj[property] ]
+				}
+				// Loop through array and create an mapped entry for each
+				for (const el of flowObj[property] ) {
+					// console.log("el" , el)
+					if (el.name) {
+						let nextNode;
+						let isNode = false;
+						switch (property) {
+							case 'loops':
+								isNode = true;
+								nextNode = (el.connector) ? el.connector.targetReference : null;
+								break;					
+							default:
+								if (el.connector) {
+									isNode = true;
+									nextNode =  el.connector.targetReference;
+								}
+								break;
+						}
+
+						if (isNode) {
+							const mappedEl = {
+								name: el.name,
+								label: el.label,
+								type: property,
+								nextNode: nextNode,
+								nextValueConnector : (el.nextValueConnector) ?
+									el.nextValueConnector.targetReference : null
+							}
+							flowMap[el.name] = mappedEl;
+						}
+					}
+				  }
+				break;
+		}
+		
+	}
+	// console.log(JSON.stringify(flowObj));
+	// console.log(flowMap);
+	return flowMap;
+}
+
+function getNodeStr(node, flowMap) {
+	console.log("getNodeStr", node);
+	switch (node.type) {
+		case 'actionCalls' :
+			return "#344568:<color:white><size:30><&pulse></size>;\nfloating note left\n**" + node.name + "**\nAction\nend note\n";
+		case 'assignments' :
+			return "#F97924:<color:white><size:30><&menu></size>;\nfloating note left\n**" + node.name + "**\nAssignment\nend note\n";
+		case 'loops':
+			let loopName = node.name;
+			console.log("in loop", loopName);
+			let nextNode = flowMap[node.nextValueConnector];
+			let bodyStr = "floating note left: " + loopName + "\n repeat :<size:30><&loop-circular></size>;\n";
+			let end = false;
+			while (!end) {
+				console.log("nextNode2", nextNode);
+				bodyStr += getNodeStr(nextNode);
+				if (!nextNode.nextNode || nextNode.nextNode === loopName) {
+					end = true;
+				} else {
+					nextNode = flowMap[nextNode.nextNode]
+				}
+			}
+			return bodyStr + "repeat while (more data?)\n";
+		default:
+			return "' " + node.name + "\n";
+	}
+}
+
 
 module.exports = {
 	activate,
