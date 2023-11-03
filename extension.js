ebe7bbdfefe77d07b7f1ce3ab62ddb1b65a71dce
Todd Halfpenny <todd@toddhalfpenny.com>
2023-11-03 21:59:13 +0000

    Started adding mermaid support

--- extension.js
+++ extension.js
@@ -3,38 +3,59 @@
 const vscode = require('vscode');
 const { XMLParser } = require("fast-xml-parser");
 
+const mode = "mermaid";
 
 const NODE_CONFIG = {
 	'actionCalls': {
 		background: "#344568",
 		color: "white",
 		label: "Action",
-		icon: "<&pulse>"
+		icon: "<&pulse>",
+		mermaidIcon: "⚡" ,
+		mermaidClose: ")",
+		mermaidOpen: "("
 	},
 	'assignments': {
 		background: "#F97924",
 		color: "white",
 		label: "Assignment",
-		icon: "<&menu>"
+		icon: "<&menu>",
+		mermaidIcon: "⚌" ,
+		mermaidClose: ")",
+		mermaidOpen: "("
 	},
 	'decisions': {
 		background: "#DD7A00",
 		color: "white",
 		label: "Decision",
-		icon: "<&fork>"
+		icon: "<&fork>",
+		mermaidIcon: "⇋" ,
+		mermaidClose: "}}",
+		mermaidOpen: "{{"
 	},
-	'loops': {},
+	'loops': {
+		background: "#E07D1C",
+		mermaidIcon: "↻",
+		mermaidClose: ")",
+		mermaidOpen: "("
+	},
 	'recordCreates': {
 		background: "#F9548A",
 		color: "white",
 		label: "Create Records",
-		icon: "<&medical-cross>"
+		icon: "<&medical-cross>",
+		mermaidIcon: "+" ,
+		mermaidClose: ")",
+		mermaidOpen: "("
 	},
 	'recordUpdates': {
 		background: "#F9548A",
 		color: "white",
 		label: "Update Records",
-		icon: "<&pencil>"
+		icon: "<&pencil>",
+		mermaidIcon: "📝" ,
+		mermaidClose: ")",
+		mermaidOpen: "("
 	},
 };
 
@@ -59,18 +80,28 @@ function deactivate() {}
 async function generatePlantUMLPreview() {
 	const {flowMap,uml} = await generatePlantUML();
 
+	let extension = (mode === 'mermaid') ? 'md' : 'puml';
 	// Create tempfile with plant UML contents
-	let setting = vscode.Uri.parse("untitled:" + flowMap['label'].replaceAll(' ', '-').toLowerCase() + ".puml");
+	let setting = vscode.Uri.parse("untitled:" + flowMap['label'].replaceAll(' ', '-').toLowerCase() + "." + extension);
 	try {
 		const doc = await vscode.workspace.openTextDocument(setting);
 		const e = await vscode.window.showTextDocument(doc, 1, false);
 		e.edit(async edit => {
 			await edit.insert(new vscode.Position(0, 0), uml);
 
-			// Run plantuml.preview (this calls Jebbs' extension)
-			setTimeout(() => {
-				vscode.commands.executeCommand("plantuml.preview");
-			}, 200);
+			switch (mode) {
+				case 'mermaid':
+					// let pathToManual = vscode.Uri.joinPath(context.extensionPath, "manual.md");
+    				// let uriManual = vscode.Uri.file(pathToManual);
+					vscode.commands.executeCommand('markdown.showPreview', setting);
+					break;
+				default:				
+					// Run plantuml.preview (this calls Jebbs' extension)
+					setTimeout(() => {
+						vscode.commands.executeCommand("plantuml.preview");
+					}, 200);
+					break;
+				}
 			
 		});
 	} catch (error) {
@@ -85,31 +116,39 @@ async function generatePlantUML() {
 	const xmlData = vscode.window.activeTextEditor.document.getText();
 	const parser = new XMLParser();
 	const flowObj = parser.parse(xmlData).Flow;
-	console.log(JSON.stringify(flowObj));
+	// console.log(JSON.stringify(flowObj));
 	const flowMap = await createFlowMap(flowObj);
 	console.log(JSON.stringify(flowMap));
 
-	const START_STR = "' THIS IS A TEMPORARY FILE\n@startuml " + flowMap['label'] + "\nstart\n";
-	const TITLE_STR = "title " + flowMap['label'] + "\n";
-	let nextNode = flowMap[flowMap['start'].connector.targetReference];
-	let end = false;
-	let bodyStr = '';
-	let i = 0;
-	while (!end && i < 10) {
-		console.log("nextNode", nextNode);
-		bodyStr += getNodeStr(nextNode, flowMap);
-		if (!nextNode.nextNode) {
-			end = true;
-		} else {
-			nextNode = flowMap[nextNode.nextNode]
-		}
-		i++;
+	switch (mode) {
+		case 'mermaid':
+			return {
+				flowMap: flowMap,
+				uml: await generateMermaidFile(flowMap)
+			}
+		default: // plantuml
+			const START_STR = "' THIS IS A TEMPORARY FILE\n@startuml " + flowMap['label'] + "\nstart\n";
+			const TITLE_STR = "title " + flowMap['label'] + "\n";
+			let nextNode = flowMap[flowMap['start'].connector.targetReference];
+			let end = false;
+			let bodyStr = '';
+			while (!end) {
+				// console.log("nextNode", nextNode);
+				bodyStr += getNodeStr(nextNode, flowMap);
+				if (!nextNode.nextNode) {
+					end = true;
+				} else {
+					nextNode = flowMap[nextNode.nextNode]
+				}
+			}
+			const END_STR = "stop\n@enduml";
+			return {
+				flowMap: flowMap,
+				uml: START_STR + TITLE_STR + bodyStr + END_STR
+			};
 	}
-	const END_STR = "stop\n@enduml";
-	return {
-		flowMap: flowMap,
-		uml: START_STR + TITLE_STR + bodyStr + END_STR
-	};
+
+
 }
 
 async function createFlowMap(flowObj) {
@@ -118,8 +157,14 @@ async function createFlowMap(flowObj) {
 		switch (property) {
 			case 'description' :
 			case 'label' :
+			case 'processType' :
+			case 'status' :
+				flowMap[property] = flowObj[property];
+				break;
 			case 'start' :
 				flowMap[property] = flowObj[property];
+				flowMap[property].type = property;
+				flowMap[property].nextNode = flowObj[property].connector.targetReference;
 				break;
 			default :
 				// If only one entry (e.g one loop) then it will be an object, not an Array, so make it an Array of one
@@ -133,7 +178,7 @@ async function createFlowMap(flowObj) {
 						let nextNode;
 						switch (property) {
 							case 'decisions':
-								nextNode = (el.defaultConnector) ? el.defaultConnector.targetReference : null;
+								nextNode = (el.defaultConnector) ? el.defaultConnector.targetReference : "END";
 								let tmpRules = (el.rules.length) ? el.rules : [el.rules];
 								el.rules2 = tmpRules.map(ruleEl =>{
 									return {
@@ -145,7 +190,7 @@ async function createFlowMap(flowObj) {
 								});
 								break;	
 							case 'loops':
-								nextNode = (el.connector) ? el.connector.targetReference : null;
+								nextNode = (el.connector) ? el.connector.targetReference : "END";
 								break;					
 							default:
 								if (el.connector) {
@@ -166,6 +211,8 @@ async function createFlowMap(flowObj) {
 								rules: el.rules2
 							}
 							flowMap[el.name] = mappedEl;
+						} else if (property === 'variables') {
+							flowMap.variables = flowObj[property];
 						}
 					}
 				  }
@@ -179,40 +226,20 @@ async function createFlowMap(flowObj) {
 }
 
 function getNodeStr(node, flowMap) {
-	console.log("getNodeStr", node);
+	// console.log("getNodeStr", node);
 	let nextNode;
 	let end;
 	switch (node.type) {
 		case 'decisions':
-			const START_STR = "switch (" + node.label + ")\n"
-			const DEFATAULT_STR = "\ncase (" + node.nextNodeLabel + ")\n";
-
-			let rulesStr = "";
-			for (const rule of node.rules ) {
-				rulesStr += "case (" + rule.label + ")\n";
-
-				nextNode = nextNode = flowMap[rule.nextNode.targetReference];
-				end = false;
-				while (!end) {
-					console.log("nextNode2", nextNode);
-					rulesStr += getNodeStr(nextNode);
-					if (!nextNode.nextNode || nextNode.nextNode === node.nextNode) {
-						end = true;
-					} else {
-						nextNode = flowMap[nextNode.nextNode]
-					}
-				}
-			}
-			const END_STR = "endswitch\n";
-			return START_STR + rulesStr + DEFATAULT_STR + END_STR;
+			return processDecisions(node, flowMap);
 		case 'loops':
 			let loopName = node.name;
-			console.log("in loop", loopName);
+			// console.log("in loop", loopName);
 			nextNode = flowMap[node.nextValueConnector];
 			let bodyStr = "floating note left: " + loopName + "\n repeat :<size:30><&loop-circular></size>;\n";
 			end = false;
 			while (!end) {
-				console.log("nextNode2", nextNode);
+				// console.log("nextNode2", nextNode);
 				bodyStr += getNodeStr(nextNode);
 				if (!nextNode.nextNode || nextNode.nextNode === loopName) {
 					end = true;
@@ -231,6 +258,119 @@ function getNodeStr(node, flowMap) {
 	}
 }
 
+function processDecisions(node, flowMap) {
+	const START_STR = "switch (" + node.label + ")\n"
+	const DEFATAULT_STR = "\ncase (" + node.nextNodeLabel + ")\n";
+
+	let nextNode;
+	let end;
+	let rulesStr = "";
+	for (const rule of node.rules ) {
+		rulesStr += "case (" + rule.label + ")\n";
+
+		nextNode = nextNode = flowMap[rule.nextNode.targetReference];
+		end = false;
+		while (!end) {
+			// console.log("nextNode2", nextNode);
+			rulesStr += getNodeStr(nextNode);
+			if (!nextNode.nextNode || nextNode.nextNode === node.nextNode) {
+				end = true;
+			} else {
+				nextNode = flowMap[nextNode.nextNode]
+			}
+		}
+	}
+	const END_STR = "endswitch\n";
+	return START_STR + rulesStr + DEFATAULT_STR + END_STR;
+}
+
+async function generateMermaidFile(flowMap) {
+	const title = "# "+ flowMap['label'] + "\n### " + flowMap['processType'] + " (*" + flowMap['status'] + "*)\n";
+	const variables = await getVariablesMd(flowMap.variables) + "\n";
+	const mdStart = "```mermaid\nflowchart TB\n";
+	const nodeDefStr = await getNodeDefStr(flowMap) + "\n\n";
+	const mdClasses = await getMermaidClasses() + "\n\n";
+	const mdBody = await getMermaidBody(flowMap) + "\n\n";
+	const mdEnd = "```\n";
+	// TODO VARIABLES ETC
+	return title + variables + mdStart + nodeDefStr + mdBody + mdClasses + mdEnd;
+}
+
+async function getMermaidBody(flowMap) {
+	let bodyStr = "";
+	for (const property in flowMap) {
+		const node = flowMap[property];
+		const type = node.type;
+		const nextNode = (node.nextNode) ? node.nextNode : "END"
+		switch (type) {
+			case 'actionCalls':
+			case 'assignments':
+			case 'recordCreates':
+			case 'recordUpdates':
+				bodyStr += node.name + " --> " + nextNode + "\n";
+				break;
+			case 'start':
+				bodyStr += "START(( START )) --> " + nextNode  + "\n";
+				break;
+			case 'decisions':
+				//rules
+				for (const rule of node.rules ) {
+					bodyStr += node.name + " --> |" + rule.label + "| " + rule.nextNode.targetReference + "\n";
+				}
+				
+				// default
+				bodyStr += node.name + " --> |" + node.nextNodeLabel + "| " + nextNode + "\n";
+				break;
+			case 'loops':
+				// console.log("in loop", loopName);
+				let loopNextNode = node.nextValueConnector;
+				bodyStr += node.name + " --> " + loopNextNode + "\n";
+				bodyStr += node.name + " ---> " + node.nextNode + "\n";
+				break;
+			default:
+				// do nothing
+				break;
+		}
+	}
+	return bodyStr;
+}
+
+async function getNodeDefStr(flowMap) {
+	let nodeDefStr = "\START(( START ))\n";
+	for (const property in flowMap) {
+		const type = flowMap[property].type;
+		switch (type) {
+			case 'actionCalls':
+			case 'assignments':
+			case 'decisions':
+			case 'loops':
+			case 'recordCreates':
+			case 'recordUpdates':
+			nodeDefStr += property + NODE_CONFIG[type].mermaidOpen + NODE_CONFIG[type].mermaidIcon + "\n" + flowMap[property].label + NODE_CONFIG[type].mermaidClose + ":::" + type + "\n"
+				break;
+			default:
+				// do nothing
+				break;
+		}
+	}
+	return nodeDefStr + "\END(( END ))\n";
+}
+
+async function getVariablesMd(vars) {
+	let vStr = "## Variables\n|Name|Datatype|Collection|Input|Output|\n|-|-|-|-|-|\n";
+	for (const v of vars) {
+		vStr += "|" + v.name + "|" + v.dataType + "|" + v.isCollection + "|" + v.isInput + "|" + v.isOutput + "|\n";
+	}
+	return vStr;
+}
+
+async function getMermaidClasses() {
+	let classStr = "";
+	for (const property in NODE_CONFIG) {
+		classStr += "classDef " + property + " fill:" + NODE_CONFIG[property].background + ",color:" + NODE_CONFIG[property].color + "\n";
+	}
+	return classStr;
+}
 
 module.exports = {
 	activate,
